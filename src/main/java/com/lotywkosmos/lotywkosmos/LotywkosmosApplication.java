37b81d31321e01c649d217bc80a9474dc6e76eee
Kamil Lobasiuk <kamil.lobasiuk@gmail.com>
2020-10-19 21:34:02 +0200

    first comm

---
+++ src/main/java/com/lotywkosmos/lotywkosmos/LotywkosmosApplication.java
@@ -0,0 +1,13 @@
+package com.lotywkosmos.lotywkosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotywkosmosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotywkosmosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lotywkosmos/lotywkosmos/LotywkosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotywkosmos.lotywkosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotywkosmosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
