6096980b4bb05ab8b3eaea4c3a395fa2fdce0ae5
Kamil Łobasiuk <Kamil.lobas@gmail.com>
2020-10-25 12:36:26 +0100

    addedd user & order service

---
+++ src/main/java/com/lotywkosmos/lotywkosmos/Service/UserService.java
@@ -0,0 +1,27 @@
+package com.lotywkosmos.lotywkosmos.Service;
+
+import com.lotywkosmos.lotywkosmos.Model.User;
+import com.lotywkosmos.lotywkosmos.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;//injection
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getSingleUser(long id) {
+        return userRepository.findById(id)
+                .orElseThrow();
+    }
+
+    public void addUserToBase(User user) {
+        userRepository.save(user);
+    }
+}
