60c4bc231aabc082cac145afcb79fdbf6e865b1a
Kamil Łobasiuk <Kamil.lobas@gmail.com>
2020-12-13 09:46:10 +0100

    changed host db, added post user method, added swagger-ui

--- src/main/java/com/lotywkosmos/lotywkosmos/Repository/OrderRepository.java
+++ src/main/java/com/lotywkosmos/lotywkosmos/Repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.lotywkosmos.lotywkosmos.Repository;
 
+import com.lotywkosmos.lotywkosmos.Model.Flight;
 import com.lotywkosmos.lotywkosmos.Model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
 }
