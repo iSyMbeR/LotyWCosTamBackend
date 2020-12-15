6badd7528bba6e347cf98d6387cd3cab9cbd4503
iSyMbeR <68368860+iSyMbeR@users.noreply.github.com>
2020-12-15 16:34:15 +0100

    Kamil (#7)
    * Changed database source
    
    * Changed database source
    
    * Changed database source
    
    * deleted summaryPrice field, type of descriptions changed from String -> TEXT
    
    * changed host db,
    added post user method,
    added swagger-ui
    
    * +Filter flights,
    +paging Users
    
    Co-authored-by: Kamil Łobasiuk <Kamil.lobas@gmail.com>

--- src/main/java/com/lotywkosmos/lotywkosmos/Controllers/FlightController.java
+++ src/main/java/com/lotywkosmos/lotywkosmos/Controllers/FlightController.java
@@ -7,8 +7,10 @@ import com.lotywkosmos.lotywkosmos.Service.PortLinkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -19,12 +21,18 @@ public class FlightController {
     private final FlightService flightService;
 
     @GetMapping("/flights")
-    public List<Flight> getPortLinks(){
+    public List<Flight> getPortLinks() {
         return flightService.getFlight();
     }
 
     @GetMapping("/flights/{id}")
-    public Flight getSingleFlight(@PathVariable long id) {
+    public Flight getSingleFlight(@PathVariable Long id) {
         return flightService.getSingleFlight(id);
+    }
+
+    @GetMapping("/flights/{startingPortName}/{endingPortName}/{startPrice}/{endPrice}/{startDate}/{endDate}")
+    public List<Flight> getFilterFlights(@PathVariable String startingPortName, @PathVariable String endingPortName, @PathVariable Double startPrice, @PathVariable Double endPrice,
+                                         @PathVariable String startDate, @PathVariable String endDate) {
+        return flightService.getFilterFlight(startingPortName, endingPortName, startPrice, endPrice, startDate, endDate);
     }
 }
--- src/main/java/com/lotywkosmos/lotywkosmos/Service/FlightService.java
+++ src/main/java/com/lotywkosmos/lotywkosmos/Service/FlightService.java
@@ -2,11 +2,15 @@ package com.lotywkosmos.lotywkosmos.Service;
 
 import com.lotywkosmos.lotywkosmos.Model.Flight;
 import com.lotywkosmos.lotywkosmos.Model.Port;
+import com.lotywkosmos.lotywkosmos.Model.PortLink;
 import com.lotywkosmos.lotywkosmos.Repository.FlightRepository;
 import com.lotywkosmos.lotywkosmos.Repository.PortRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -14,7 +18,7 @@ import java.util.List;
 public class FlightService {
     private final FlightRepository flightRepository;//injection
 
-    public List<Flight> getFlight(){
+    public List<Flight> getFlight() {
         return flightRepository.findAll();
     }
 
@@ -26,4 +30,9 @@ public class FlightService {
     public void addFlightToBase(Flight flight) {
         flightRepository.save(flight);
     }
+
+    public List<Flight> getFilterFlight(String startingPortName, String endingPortName, Double startPrice, Double endPrice, String startDate, String endDate) {
+        return flightRepository.getAllByStart_dateAndEnd_date(startingPortName, endingPortName, startPrice, endPrice, LocalDate.parse(startDate), LocalDate.parse(endDate));
+    }
 }
+
