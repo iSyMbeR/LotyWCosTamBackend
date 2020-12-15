f8de42dc442cecb0e744f9f2c4f79d69a1e78ab1
Kamil Łobasiuk <Kamil.lobas@gmail.com>
2020-12-15 21:53:27 +0100

    added @CrossOrigin(origins = "*", allowedHeaders = "*")

--- src/main/java/com/lotywkosmos/lotywkosmos/Controllers/FlightController.java
+++ src/main/java/com/lotywkosmos/lotywkosmos/Controllers/FlightController.java
@@ -5,16 +5,14 @@ import com.lotywkosmos.lotywkosmos.Model.PortLink;
 import com.lotywkosmos.lotywkosmos.Service.FlightService;
 import com.lotywkosmos.lotywkosmos.Service.PortLinkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class FlightController {
 
 
--- src/main/java/com/lotywkosmos/lotywkosmos/Controllers/UserController.java
+++ src/main/java/com/lotywkosmos/lotywkosmos/Controllers/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
 
